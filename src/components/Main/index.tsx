01bd2551ef1c44fd6b6e57fbc3943b7eff27dc5c
Marcos Salazar <Marcos1010521@gmail.com>
2021-04-02 19:03:25 -0400

    preconfi/config-jest-react-testing-library

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+const Main = () => (
+  <main>
+    <h1>One Price Games</h1>
+  </main>
+)
+
+export default Main
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-
+/* eslint react/prop-types: 0 */
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
