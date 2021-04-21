8d608e9603ab54287c57cbbc3db5eab6cb5be807
Marcos Salazar <Marcos1010521@gmail.com>
2021-04-21 14:04:14 -0400

    boilertplate/fix storybook - use controls and essential addons

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,17 +1,17 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'One Price Games',
+  description = 'A boilerplate for a website using NextJS + Typescript + Styled Components!',
+}) => (
   <S.Wrapper>
     <S.ShadowSquare>
       <S.Logo
         src="/img/jollyRoger.png"
         alt="logo, desenho de um crânio com chapéu de palha e dois ossos cruzados, como numa  bandeira pirata"
       />
-      <S.Title>One Price Games</S.Title>
-      <S.Description>
-        A boilerplate for a website using NextJS + Typescript + Styled
-        Components!
-      </S.Description>
+      <S.Title>{title}</S.Title>
+      <S.Description>{description}</S.Description>
       <S.Illustration
         src="/img/onepiecelogo.png"
         alt="logo do desenho one piece, uma caveira de pirata com um chapéu de palha e o nome   one piece ao lado"
